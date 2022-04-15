175b33bc03fe08d81c5469da54e02c6795869e7c
unknown <fadonic@yahoo.com>
2022-04-15 09:06:52 +0100

    Added basic test case for validate Ip

--- server.js
+++ server.js
@@ -1,39 +1,4 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const morgan = require('morgan')
-const helmet = require('helmet')
-
-dotenv.config()
-
-// Connect to database
-const dbcon = (() => {
-  try {
-    const resonse = mongoose.connect(process.env.MONGO_URL_OFFLINE, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    console.log('connected to database')
-  } catch (err) {
-    console.log('connection error ' + err)
-  }
-})()
-
-const app = express()
-
-// configure middlewares
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(helmet())
-app.use(morgan('common'))
-
-const gatewaysRoute = require('./routes/gateways')
-const devicesRoute = require('./routes/devices')
-
-app.use('/api/gateways', gatewaysRoute)
-app.use('/api/devices', devicesRoute)
+const app = require('./app')
 
 const PORT = process.env.PORT || 8000
 
--- test/validateIPAddress.test.js
+++ test/validateIPAddress.test.js
@@ -1,3 +1,18 @@
+const { expectCt } = require('helmet')
 const { validateIPAddress } = require('../util')
 
-//console.log(validateIPAddress('') === false)
+test('should be false for empty ip', () => {
+  expect(validateIPAddress('')).toBe(false)
+})
+
+test('should be false if ip block number is greater than 255', () => {
+  expect(validateIPAddress('277.88.10.1')).toBe(false)
+})
+
+test('should be false if ip block number start with 0', () => {
+  expect(validateIPAddress('256.88.10.1')).toBe(false)
+})
+
+test('should be true if ip pass all validations', () => {
+  expect(validateIPAddress('172.16.254.1')).toBe(true)
+})
